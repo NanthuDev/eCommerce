7750cbe934336178023112ecb198b655f5ce6af9
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2025-03-13 23:45:12 +0800

    user service

--- backend/user-service/src/main/java/com/ecommerce/user_service/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/ecommerce/user_service/UserServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.eCommerce;
+package com.ecommerce.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
